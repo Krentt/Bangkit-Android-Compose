30fe1a6e59ae2e6011551d9e8359069b36261be9
Marvel <marvelphannn@gmail.com>
2023-05-17 01:18:52 +0700

    add jetreward

---
+++ JetReward(starter)/app/src/main/java/com/dicoding/jetreward/ui/navigation/Navigationitem.kt
@@ -0,0 +1,11 @@
+package com.dicoding.jetreward.ui.navigation
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class Navigationitem(
+    val title: String,
+    val icon: ImageVector,
+    val screen: Screen
+)
+
+
